42f3e751b2a0ab5768658bc56a023b738b7e4582
VitalikMetallik <flash_PS4@mail.ru>
2024-09-04 22:55:14 +0300

    3rd step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,20 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin;
+
+import java.sql.SQLException;
 
 public class App {
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "8080");
-        return Integer.valueOf(port);
+        return Integer.parseInt(port);
+    }
+
+    private static String getDatabaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
     }
 
     public static void main(String[] args) {
@@ -15,6 +23,19 @@ public class App {
     }
 
     public static Javalin getApp() {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDatabaseUrl());
+
+        try (
+                var dataSource = new HikariDataSource(hikariConfig);
+                var connection = dataSource.getConnection();
+                var statement = connection.createStatement()
+        ) {
+            statement.execute("sql");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,9 @@
+package hexlet.code.model;
+
+import java.time.LocalDate;
+
+public class Url {
+    private Long id;
+    private String name;
+    private LocalDate createdAt;
+}
