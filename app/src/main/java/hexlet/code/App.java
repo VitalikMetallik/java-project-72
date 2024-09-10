c392f4fa49d966ed585eef6f33eb414f38466160
VitalikMetallik <flash_PS4@mail.ru>
2024-09-10 22:08:21 +0300

    4th step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,18 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
 public class App {
-
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "8080");
         return Integer.parseInt(port);
@@ -15,6 +21,13 @@ public class App {
 
     private static String getDatabaseUrl() {
         return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 
     public static void main(String[] args) {
@@ -26,22 +39,30 @@ public class App {
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
 
+        String sql = null;
+        try {
+            sql = Files.readString(Paths.get("init.sql"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         try (
                 var dataSource = new HikariDataSource(hikariConfig);
                 var connection = dataSource.getConnection();
-                var statement = connection.createStatement()
+                var statement = connection.createStatement();
         ) {
-            statement.execute("sql");
+            statement.execute(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
         app.get("/", ctx -> {
-            ctx.result("Hello World");
+            ctx.render("index.html");
         });
 
         return app;
